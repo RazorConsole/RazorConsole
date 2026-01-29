a2045089624c3a92c2097e4179f95bc9b4804643
Marcin Lach <marcin.lach.90@gmail.com>
2026-01-29 18:13:54 +0100

    docs:multipage example app (with align-center issues)

---
+++ examples/MultipageApp/Program.cs
@@ -0,0 +1,12 @@
+// Copyright (c) RazorConsole. All rights reserved.
+
+using Microsoft.Extensions.Hosting;
+using RazorConsole.Core;
+
+var builder = Host.CreateDefaultBuilder(args)
+    .UseRazorConsole<MultipageApp.Components.App>();
+
+var host = builder.Build();
+
+await host.RunAsync();
+
