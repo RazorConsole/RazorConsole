679c3e203df11cc75f7fac2a0943d78bf0035bae
Marcin Lach <marcin.lach.90@gmail.com>
2026-02-01 11:06:41 +0100

    unicode rendering fix

--- examples/MultipageApp/Program.cs
+++ examples/MultipageApp/Program.cs
@@ -3,6 +3,9 @@
 using Microsoft.Extensions.Hosting;
 using RazorConsole.Core;
 
+// Set console encoding to UTF-8 for Unicode symbols
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
 var builder = Host.CreateDefaultBuilder(args)
     .UseRazorConsole<MultipageApp.Components.App>();
 
